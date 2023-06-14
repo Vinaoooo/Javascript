99a5ce9dbed8236edf2373b02dc31213e0c2f287
Vinao <vinicius250703@gmail.com>
2023-06-14 15:26:59 -0300

    first commit for new code Javascript

---
+++ index.js
@@ -0,0 +1,35 @@
+// console.log para printar algo na tela
+console.log("---------------------------------")
+
+// Para criar uma variavel usa-se LET
+let nome = "Vinicius";
+
+// Uma variavel pode ser alterada no decorrer do programa 
+nome = "Mudou o nome"
+
+// usa-se const ao inves de let para variaveis que não irão mudar 
+const idade = 20;
+
+let nome1 = "Vinicius"
+
+console.log(idade)
+console.log("Como você está "+ nome1 + "?")
+
+console.log("---------------------------------")
+const nome_mercado = "Supermercado Leão"
+const endereço = "Rua Tabatinga"
+let preco_do_leite = 9.5;
+let preco_do_queijo = 30;
+
+console.log("Promoção!!!")
+console.log("Somente no " + nome_mercado)
+console.log("Preço do leite: " + preco_do_leite)
+console.log("Preço do queijo: " + preco_do_queijo)
+console.log("Localizado na " + endereço)
+
+console.log("---------------------------------")
+// console.log executa contas dentro dele 
+console.log("O valor da primeira equação é " + (10 * (25 + 5) / 5 + (58 + 6)))
+
+let num_1 = 10;
+console.log("O valor da segunda equação é " + (num_1 + 35 * (59 * 2 * 3)))
